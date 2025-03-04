78ac1b31b7b88896fe6db8b66b1e7eef1ba1b9e1
luismrtnzgl <luis.mrtnzgl@gmail.com>
2025-03-04 21:06:16 +0100

    Martes 4

--- 05_Streamlit/prediccion_nuevo.py
+++ 05_Streamlit/prediccion_nuevo.py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 # Asegurar rutas absolutas para Streamlit Cloud
 BASE_DIR = os.path.dirname(__file__)
-MODEL_PATH = os.path.join(BASE_DIR, "03_EDA/stacking_model.pkl")
+MODEL_PATH = os.path.join(BASE_DIR, "03_EDA/stacking_model_compressed.pkl")
 PREPROCESS_PATH = os.path.join(BASE_DIR, "03_EDA/preprocessing.pkl")
 DATA_PATH = os.path.join(BASE_DIR, "01_Data_Cleaning/df_lego_final_venta.csv")
 
