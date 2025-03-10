183843a9ae80b4675b2a620d2c883c3599ea2933
Erove13 <ema.rosas.v@gmail.com>
2025-03-10 14:14:39 +0100

    colors

--- 08_APP_U/app_def.py
+++ 08_APP_U/app_def.py
@@ -65,11 +65,10 @@ with st.sidebar.container():
 
 with st.sidebar:
 
-    st.image("08_APP_U/IRONBRICK_APP_1_PEQ.png", use_column_width=True)
     app = option_menu(
         menu_title="",
         options=["Recomendador de Inversión en sets Actuales", "Recomendador de Inversión en sets Retirados","Alertas de Telegram", "Identificador de Sets"],
-        icons=["08_APP_U\IRONBRICK_APP_1_PEQ.png"],
+        menu_icon=["08_APP_U\IRONBRICK_APP_1_PEQ.png"],
         styles = {"container": {"nav-link": {"background-color": "ffef47"}}}
     )
 
