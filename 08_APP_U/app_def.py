363fba53de20a045b085833cd6aafca3aa42f3d0
Erove13 <ema.rosas.v@gmail.com>
2025-03-10 17:36:33 +0100

    colors

--- 08_APP_U/app_def.py
+++ 08_APP_U/app_def.py
@@ -52,7 +52,7 @@ with st.sidebar:
         options=["Recomendador de Inversión en sets Actuales", "Recomendador de Inversión en sets Retirados","Alertas de Telegram", "Identificador de Sets"],
         icons=["📌", "📌", "📌", "📌"],
         styles = {
-            "container": {"background-color": "#ffef47", "margin": "50px"},
+            "container": {"background-color": "#ffef47", "margin-top": "20px"},
             "nav-link": {"background-color": "#92cfce", "color": "black"},
             "nav-link-selected": {"background-color": "#e87577", "color": "white"}
 
