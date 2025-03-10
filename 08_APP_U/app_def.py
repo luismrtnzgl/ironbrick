4114d910858a3fa2c8d4b5f09f1065af4bb1caf5
Erove13 <ema.rosas.v@gmail.com>
2025-03-10 15:29:34 +0100

    colors

--- 08_APP_U/app_def.py
+++ 08_APP_U/app_def.py
@@ -151,7 +151,7 @@ df_lego = load_data()
 # ✅ Muestra la página seleccionada
 if st.session_state.page == "Recomendador de Inversión en sets Actuales":
     # Agregar una imagen de cabecera
-    st.image("08_APP_U/IRONBRICK_APP_1_PEQ.png", use_container_width=True)
+    st.image("08_APP_U/IRONBRICK_APP_1_PEQ.png", width="50")
 
     st.title("Recomendador de sets actuales para Inversión en LEGO 📊")
 
